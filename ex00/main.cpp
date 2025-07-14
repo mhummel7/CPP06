73c3ba89cd440a561ef29814faf0c4ada7e054ba
Manuel <mhummel@student.42heilbronn.de>
2025-07-14 11:01:27 +0200

    Update main.cpp
    Small update to make it more clear

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,19 +6,18 @@
 /*   By: mhummel <mhummel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 09:25:17 by mhummel           #+#    #+#             */
-/*   Updated: 2025/06/25 10:34:37 by mhummel          ###   ########.fr       */
+/*   Updated: 2025/07/14 08:35:14 by mhummel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 #include <iostream>
-#include <sstream>
 
 int	main(int argc, char *argv[])
 {
 	if (argc != 2)
 	{
-		std::cerr << "Usage: " << argv[0] << " <literal>" << std::endl;
+		std::cout << "Usage: ./convert <literal>" << std::endl;
 		return (1);
 	}
 
