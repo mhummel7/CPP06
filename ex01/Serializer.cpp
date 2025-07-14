f8d3e9984e6594f1a99c7cdd653683fa2b006e2c
Manuel <mhummel@student.42heilbronn.de>
2025-07-14 11:04:14 +0200

    Create Serializer.cpp
    Implement serialize and deserialize using reinterpret_cast

---
+++ ex01/Serializer.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Serializer.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhummel <mhummel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/14 09:46:38 by mhummel           #+#    #+#             */
+/*   Updated: 2025/07/14 10:06:24 by mhummel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// Serializer.cpp
+#include "Serializer.hpp"
+
+uintptr_t Serializer::serialize(Data *ptr)
+{
+	return (reinterpret_cast<uintptr_t>(ptr));
+}
+
+Data *Serializer::deserialize(uintptr_t raw)
+{
+	return (reinterpret_cast<Data *>(raw));
+}
